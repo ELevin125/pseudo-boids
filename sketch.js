d6f0ab88f44d3e2fd36ac862c030fa5535df697d
Emil <emil.levin125@gmail.com>
2024-06-12 11:57:45 +0400

    cleanup

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ let MANAGER;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    MANAGER = new Manager(25, color(255, 100, 0));
+    const boidCount = 25;
+    const boidColor = color(255, 100, 0);
+    MANAGER = new Manager(boidCount, boidColor);
 }
 
 function draw() {
@@ -14,20 +16,25 @@ function draw() {
 
 class Manager {
     constructor(boidCount = 10, color) {
-        this.direction = p5.Vector.random2D();
-        this.boids = this._createBoids(boidCount, color);
+        this.direction = p5.Vector.random2D(); // overall direction of the boids
+
+        const size = 20;
+        const paddingScalar = 3;
+        this.boids = this._createBoids(boidCount, size, paddingScalar, color);
     }
 
-    _createBoids(boidCount, color) {
+    _createBoids(boidCount, size, paddingScalar, color) {
         return Array.from({ length: boidCount }, () => new Boid(random(width / 2), 
                                                                 random(height / 2), 
-                                                                20,
+                                                                size,
                                                                 color,
-                                                                3,
+                                                                paddingScalar,
                                                                 this.direction));
     }
 
     update() {
+        // change direction slightly to let the boids' movement seem more random over time
+        // make sure mult value is low, to prevent rapidly changing directions
         let perturbation = p5.Vector.random2D().mult(0.1);
         this.direction.add(perturbation);
 
@@ -40,33 +47,34 @@ class Boid {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.color = color
+        this.color = color;
         this.paddingScalar = paddingScalar;
 
         this.mainDirection = mainDirection.copy();
-        this.direction = p5.Vector.add(this.mainDirection, p5.Vector.random2D().mult(0.1));
-        this.prevDirection = this.direction.copy();
+        this._direction = p5.Vector.add(this.mainDirection, p5.Vector.random2D().mult(0.1));
+        this._prevDirection = this._direction.copy();
 
-        this.currentRandom = p5.Vector.random2D().mult(0.1);
+        this._currentRandom = p5.Vector.random2D().mult(0.1);
     }
 
     update(mainDirection, boids) {
-        this.currentRandom.add(p5.Vector.random2D().mult(0.03));
-        let newDirection = mainDirection.copy().add(this.currentRandom);
+        this._currentRandom.add(p5.Vector.random2D().mult(0.03));
+        let newDirection = mainDirection.copy().add(this._currentRandom);
     
+        // slightly adjust the direction to be away from any intersecting boids
         boids.forEach(other => {
             if (other !== this && this._isWithinPadding(other)) {
                 let avoidance = p5.Vector.sub(this._getPositionVector(), other._getPositionVector());
                 avoidance.normalize();
-                newDirection.add(avoidance.mult(0.4)); // Small perturbation
+                newDirection.add(avoidance.mult(0.4));
             }
         });
     
         newDirection.normalize();
-        // Smooth transition between previous and new direction
-        this.direction = p5.Vector.lerp(this.prevDirection, newDirection, 0.03);
-    
-        this.prevDirection = this.direction.copy();
+
+        // prevents jittering 
+        this._direction = p5.Vector.lerp(this._prevDirection, newDirection, 0.03);
+        this._prevDirection = this._direction.copy();
     
         this._move();
         this._draw();
@@ -83,8 +91,8 @@ class Boid {
     }
 
     _move() {
-        this.x += this.direction.x;
-        this.y += this.direction.y;
+        this.x += this._direction.x;
+        this.y += this._direction.y;
 
         // Handle edge wrapping
         if (this.x > width) this.x = 0;
@@ -98,9 +106,10 @@ class Boid {
         noStroke()
         fill(this.color);
         translate(this.x, this.y);
-        rotate(this.direction.heading());
+        rotate(this._direction.heading());
         let s = this.size / 2;
         triangle(s * -1, s * 1, s * -1, s * -1, s * 1.5, s * 0);
+        // draw debug
         // noFill();
         // stroke(0, 255, 0);
         // ellipse(0, 0, this.size * this.paddingScalar, this.size * this.paddingScalar);
