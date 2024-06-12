8e77c69d37383399a5701cb493b996a3924a6001
Emil <emil.levin125@gmail.com>
2024-06-12 11:24:46 +0400

    Boids avoid each other, and head in roughly the same direction

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let MANAGER;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    MANAGER = new Manager(10)
+    MANAGER = new Manager(50);
 }
 
 function draw() {
@@ -14,34 +14,93 @@ function draw() {
 
 class Manager {
     constructor(boidCount = 10) {
-        this.boids = this._createBoids(boidCount)
+        this.direction = p5.Vector.random2D();
+        this.boids = this._createBoids(boidCount);
     }
 
     _createBoids(boidCount) {
-        return Array.from({ length: boidCount }, () => new Boid(random(width), random(height), 10));
+        return Array.from({ length: boidCount }, () => new Boid(random(width / 2), 
+                                                                random(height / 2), 
+                                                                20,
+                                                                5,
+                                                                this.direction));
     }
 
     update() {
-        this.boids.forEach(boid => boid.update());
+        let perturbation = p5.Vector.random2D().mult(0.1);
+        this.direction.add(perturbation);
+
+        this.boids.forEach(boid => boid.update(this.direction, this.boids));
     }
 }
 
 class Boid {
-    constructor(x, y, size) {
+    constructor(x, y, size, paddingScalar, mainDirection) {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.paddingScalar = paddingScalar;
+
+        this.mainDirection = mainDirection.copy();
+        this.direction = p5.Vector.add(this.mainDirection, p5.Vector.random2D().mult(0.1));
+        this.prevDirection = this.direction.copy();
+
+        this.currentRandom = p5.Vector.random2D().mult(0.1);
     }
 
-    update() {
+    update(mainDirection, boids) {
+        this.currentRandom.add(p5.Vector.random2D().mult(0.03));
+        let newDirection = mainDirection.copy().add(this.currentRandom);
+    
+        boids.forEach(other => {
+            if (other !== this && this._isWithinPadding(other)) {
+                let avoidance = p5.Vector.sub(this._getPositionVector(), other._getPositionVector());
+                avoidance.normalize();
+                newDirection.add(avoidance.mult(0.4)); // Small perturbation
+            }
+        });
+    
+        newDirection.normalize();
+        // Smooth transition between previous and new direction
+        this.direction = p5.Vector.lerp(this.prevDirection, newDirection, 0.03);
+    
+        this.prevDirection = this.direction.copy();
+    
+        this._move();
         this._draw();
+    }
+    
+
+    _isWithinPadding(other) {
+        let d = dist(this.x, this.y, other.x, other.y);
+        return d < this.size * this.paddingScalar;
+    }
+
+    _getPositionVector() {
+        return createVector(this.x, this.y);
+    }
+
+    _move() {
+        this.x += this.direction.x;
+        this.y += this.direction.y;
+
+        // Handle edge wrapping
+        if (this.x > width) this.x = 0;
+        if (this.x < 0) this.x = width;
+        if (this.y > height) this.y = 0;
+        if (this.y < 0) this.y = height;
     }
 
     _draw() {
         push();
         fill(255);
         translate(this.x, this.y);
-        triangle(15, 37.5, 29, 10, 43, 37.5);
+        rotate(this.direction.heading());
+        let s = this.size / 2;
+        triangle(s * -1, s * 1, s * -1, s * -1, s * 1.5, s * 0);
+        // noFill();
+        // stroke(0, 255, 0);
+        // ellipse(0, 0, this.size * this.paddingScalar, this.size * this.paddingScalar);
         pop();
     }
 }
